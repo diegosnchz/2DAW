f5a0277488835fc5f29eccf6a017ecee5e1ed825
diego <diegovscodee@gmail.com>
2024-11-08 16:17:23 +0100

    subo cambios por si acaso.. he arreglado el tema del idioma ya por fin, ahora solo falta el botón de información del carrusel y el efecto typewrite

---
+++ Portfolio/traduction.js
@@ -0,0 +1,54 @@
+// Función para cargar las traducciones desde el archivo JSON
+async function loadTranslations(lang) {
+  try {
+      const response = await fetch('translations.json');
+      const translations = await response.json();
+      applyTranslations(translations[lang]);
+  } catch (error) {
+      console.error("Error loading translations:", error);
+  }
+}
+
+// Función para aplicar las traducciones en la página
+function applyTranslations(translations) {
+  document.querySelector('h1').textContent = translations.title;
+  document.querySelector('a[href="#"]').textContent = translations.home;
+  document.querySelector('a[href="#gallery"]').textContent = translations.gallery;
+  document.querySelector('#skills h2').textContent = translations.skills;
+  document.querySelector('#skills p').textContent = translations.description;
+
+  // CARRUSEL - GALERÍA
+  document.querySelector('.tituloProyecto1').textContent = translations.proyecto.title[0];
+  document.querySelector('.descripcionProyecto1').textContent = translations.proyecto.description[0];
+
+  document.querySelector('.tituloProyecto2').textContent = translations.proyecto.title[1];
+  document.querySelector('.descripcionProyecto2').textContent = translations.proyecto.description[1];
+
+  document.querySelector('.tituloProyecto3').textContent = translations.proyecto.title[2];
+  document.querySelector('.descripcionProyecto3').textContent = translations.proyecto.description[2];
+
+  document.querySelector('.tituloProyecto4').textContent = translations.proyecto.title[3];
+  document.querySelector('.descripcionProyecto4').textContent = translations.proyecto.description[3];
+
+  document.querySelector('#brand').textContent = "DiegoDev"; // asi hago que el script no cambie el titulo
+  document.querySelector('a[href="#gallery"]').textContent = translations.gallery;
+
+}
+
+// Event listeners para cambiar el idioma
+document.getElementById('esp').addEventListener('change', function () {
+  if (this.checked) {
+      loadTranslations('es');
+  }
+});
+
+document.getElementById('ing').addEventListener('change', function () {
+  if (this.checked) {
+      loadTranslations('en');
+  }
+});
+
+// Cargar el idioma por defecto al cargar la página
+window.onload = function() {
+  loadTranslations('en');
+};
