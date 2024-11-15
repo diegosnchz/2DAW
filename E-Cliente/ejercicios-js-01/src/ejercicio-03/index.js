06f8e53e07b001430451aba4228db740601f7b27
diego <diegovscodee@gmail.com>
2024-11-15 17:38:12 +0100

    ejercicios hechos js menos el ultimo

--- E-Cliente/ejercicios-js-01/src/ejercicio-03/index.js
+++ E-Cliente/ejercicios-js-01/src/ejercicio-03/index.js
@@ -4,7 +4,7 @@
   let num1=10;
 // 2. Utiliza el operador ternario para asignar a la variable `tipoNumero` el valor "positivo" si `numero` es mayor que 0, y "negativo o cero" si no lo es.
 //No hace falta que esté metido en una función pero si está mejor que mejor
-  num>0 ? "positivo" : "negativo";
+  num >0 ? "positivo" : "negativo";
 // 3. Declara una variable `cadena` y asígnale un valor de tipo cadena de texto.
   let cadena = 'cadena';
 // 4. Utiliza el operador ternario para asignar a la variable `longitudCadena` indicar que la cadena es "larga" si su longitud es mayor que 5, y "corta" si no lo es.
